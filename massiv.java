a3f281307e31449ecb5b61c3dffb39172af4982d
soykasloyka <petr.emelianov@icloud.com>
2022-06-27 20:43:46 +0300

    mass

---
+++ massiv.java
@@ -0,0 +1,60 @@
+public class Solution1 {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
+
+public class Solution2 {
+    public static final String ODD = "Нечётный";
+    public static final String EVEN = "Чётный";
+    public static String[] strings = new String[5];
+
+    public static void main(String[] args) {
+        strings[0] = EVEN;
+        strings[1] = ODD;
+        strings[2] = EVEN;
+        strings[3] = ODD;
+        strings[4] = EVEN;
+
+        System.out.print("index = 0");
+        System.out.println(" value = " + strings[0]);
+        System.out.print("index = 1");
+        System.out.println(" value = " + strings[1]);
+        System.out.print("index = 2");
+        System.out.println(" value = " + strings[2]);
+        System.out.print("index = 3");
+        System.out.println(" value = " + strings[3]);
+        System.out.print("index = 4");
+        System.out.println(" value = " + strings[4]);
+    }
+}
+
+public class Solution3 {
+
+    public static int[] array = new int[]{-1, 2, 3, -4, -5};
+
+    public static void main(String[] args) {
+        if (array[0] % 2 == 0) {
+            array[0] = -array[0];
+        }
+        if (array[1] % 2 == 0) {
+            array[1] = -array[1];
+        }
+        if (array[2] % 2 == 0) {
+            array[2] = -array[2];
+        }
+        if (array[3] % 2 == 0) {
+            array[3] = -array[3];
+        }
+        if (array[4] % 2 == 0) {
+            array[4] = -array[4];
+        }
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+}
