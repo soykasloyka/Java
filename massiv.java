dcb67c63ffbf37c982cbe3e940e088fdf07f4bb4
soykasloyka <petr.emelianov@icloud.com>
2022-07-08 11:14:59 +0300

    array9

--- massiv.java
+++ massiv.java
@@ -154,3 +154,20 @@ public class Solution8 {
         System.out.println(max);
     }
 }
+
+public class Solution9 {
+
+    public static int[][][] multiArray = new int[][][]{{{4, 8, 15}, {16}}, {{23, 42}, {}}, {{1}, {2}, {3}, {4, 5}}};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < multiArray.length; i++) {
+            for (int j = 0; j < multiArray[i].length; j++) {
+                for (int k = 0; k < multiArray[i][j].length; k++) {
+                    System.out.print(multiArray[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("----------");
+        }
+    }
+}
