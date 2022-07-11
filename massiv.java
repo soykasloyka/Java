5093f9e977262ddd35d2bedc44fdd9da9290c85e
soykasloyka <petr.emelianov@icloud.com>
2022-07-11 11:09:46 +0300

    10

--- massiv.java
+++ massiv.java
@@ -171,3 +171,13 @@ public class Solution9 {
         }
     }
 }
+
+public class Solution10 {
+    public static String[] strings = new String[]{"I", "love", "Java"};
+    public static int[] ints = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(ints));
+    }
+}
