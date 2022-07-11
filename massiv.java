61b8498bc0227533f066a4bc3bba1bac12a2ac74
soykasloyka <petr.emelianov@icloud.com>
2022-07-11 11:17:48 +0300

    11

--- massiv.java
+++ massiv.java
@@ -181,3 +181,25 @@ public class Solution10 {
         System.out.println(Arrays.toString(ints));
     }
 }
+
+public class Solution11 {
+    public static String robotank = "☖";
+    public static String empty = "_";
+    public static String hit = "🎯";
+    public static int width = 30;
+    public static int height = 10;
+    public static String[][] field = new String[height][width];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < field.length; i++) {
+            Arrays.fill(field[i], empty);
+        }
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print(field[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
