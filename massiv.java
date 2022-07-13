bd13aad46790bc704e3fc4dd09245f493c976c85
soykasloyka <petr.emelianov@icloud.com>
2022-07-13 12:37:14 +0300

    13т

--- massiv.java
+++ massiv.java
@@ -247,3 +247,16 @@ public class Solution12 {
         System.out.println(Arrays.toString(array));
     }
 }
+
+public class Solution13 {
+
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    public static int element = 5;
+
+    public static void main(String[] args) {
+        int[] copy = Arrays.copyOf(array, array.length);
+        Arrays.sort(copy);
+        int index = Arrays.binarySearch(copy, element);
+        System.out.println(index >= 0);
+    }
+}
