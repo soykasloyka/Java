7734743a6ab7e1ba7a41d577651e033109db7a30
soykasloyka <petr.emelianov@icloud.com>
2022-08-26 14:03:44 +0300

    flag

---
+++ flag.java
@@ -0,0 +1,14 @@
+public class flag {
+
+    public static int setFlag(int number, int flagPos) {
+        return number | (1 << flagPos);
+    }
+
+    public static int resetFlag(int number, int flagPos) {
+        return number & ~(1 << flagPos);
+    }
+
+    public static boolean checkFlag(int number, int flagPos) {
+        return (number & (1 << flagPos)) == (1 << flagPos);
+    }
+}
