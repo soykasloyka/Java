c107c8005c16238b0daaa0729989f32ad4c425d6
soykasloyka <petr.emelianov@icloud.com>
2022-08-27 12:14:10 +0300

    c3

---
+++ converter3.java
@@ -0,0 +1,35 @@
+public class converter3 {
+    private static final String HEX = "0123456789abcdef";
+
+    public static void main(String[] args) {
+        int decimalNumber = 1256;
+        System.out.println("Десятичное число " + decimalNumber + " равно шестнадцатеричному числу " + toHex(decimalNumber));
+        String hexNumber = "4e8";
+        System.out.println("Шестнадцатеричное число " + hexNumber + " равно десятичному числу " + toDecimal(hexNumber));
+    }
+
+    public static String toHex(int decimalNumber) {
+        String hexNumber = "";
+        if (decimalNumber <= 0) {
+            return hexNumber;
+        }
+
+        while (decimalNumber != 0) {
+            hexNumber = HEX.charAt(decimalNumber % 16) + hexNumber;
+            decimalNumber = decimalNumber / 16;
+        }
+        return hexNumber;
+    }
+
+    public static int toDecimal(String hexNumber) {
+        int decimalNumber = 0;
+        if (hexNumber == null) {
+            return decimalNumber;
+        }
+
+        for (int i = 0; i < hexNumber.length(); i++) {
+            decimalNumber = 16 * decimalNumber + HEX.indexOf(hexNumber.charAt(i));
+        }
+        return decimalNumber;
+    }
+}
