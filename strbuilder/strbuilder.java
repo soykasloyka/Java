853895645a57aef2032928d6e78e1533a26eb698
soykasloyka <petr.emelianov@icloud.com>
2022-09-01 19:53:51 +0300

    str

---
+++ strbuilder/strbuilder.java
@@ -0,0 +1,21 @@
+public class strbuilder {
+    public static void main(String[] args) {
+        String string = "Учиться, учиться и еще раз учиться! ";
+
+        System.out.println(addTo(string, new String[]{"Под ", "лежачий ", "камень ", "вода ", "не ", "течет"}));
+        System.out.println(replace(string, ", ", 16, 27));
+    }
+
+    public static StringBuilder addTo(String string, String[] strings) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        for (int i = 0; i < strings.length; i++) {
+            stringBuilder.append(strings[i]);
+        }
+        return stringBuilder;
+    }
+
+    public static StringBuilder replace(String string, String str, int start, int end) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        return stringBuilder.replace(start, end, str);
+    }
+}
