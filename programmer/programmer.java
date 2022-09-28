722e993ea566e0361ed219a9b60eb5f75de2b6fb
soykasloyka <petr.emelianov@icloud.com>
2022-09-28 08:40:33 +0300

    pr

---
+++ programmer/programmer.java
@@ -0,0 +1,13 @@
+public class Programmer {
+    private int salary = 1000;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        if(this.salary < salary) {
+            this.salary = salary;
+        }
+    }
+}
