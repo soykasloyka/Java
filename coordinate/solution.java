fa7614c3438e0739c558e35db2427662b71ee4b7
soykasloyka <petr.emelianov@icloud.com>
2022-10-02 21:17:50 +0300

    New task

---
+++ coordinate/solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Coordinate coordinate = new Coordinate();
+        coordinate.setX(15);
+        coordinate.setY(25);
+        System.out.println(coordinate.getX());
+        System.out.println(coordinate.getY());
+    }
+}
