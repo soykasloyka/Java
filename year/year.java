a005e649330bd384ac21fda4a8c0af3b470bbaa0
soykasloyka <petr.emelianov@icloud.com>
2022-10-04 11:32:47 +0300

    Current year

---
+++ year/year.java
@@ -0,0 +1,19 @@
+import java.util.Calendar;
+
+public class Solution {
+
+    private int currentYear;
+
+    public Solution() {
+        this.currentYear = Calendar.getInstance().get(Calendar.YEAR);
+    }
+
+    public int getCurrentYear() {
+        return currentYear;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.getCurrentYear());
+    }
+}
