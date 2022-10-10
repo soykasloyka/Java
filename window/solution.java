82174cdbd5b4f03b8e830a6996694d0c154374cf
soykasloyka <petr.emelianov@icloud.com>
2022-10-10 12:33:07 +0300

    window task

---
+++ window/solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.printSize();
+        window.changeSize(4, 5);
+        window.printSize();
+    }
+}
