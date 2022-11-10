58dfbaf2638e7fba11f383ab020f6cea34f5990d
soykasloyka <petr.emelianov@icloud.com>
2022-11-10 12:18:07 +0200

    Equal script

---
+++ Small programms/Equal/equal.java
@@ -0,0 +1,11 @@
+public class equal {
+    static Integer first = 1000;
+    static Integer second = 1000;
+    static int third = 1000;
+
+    public static void main(String[] args) {
+        System.out.println(first.equals(second));
+        System.out.println(third == second);
+        System.out.println(third == first);
+    }
+}
